8bd258b89d32ba91b87bc12c69779ea2029896aa
Slikva <vasavislikva@gmail.com>
2026-08-04 15:44:50 +0530

    Configured Firebase Admin using environment variables

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,10 +1,13 @@
-const { initializeApp, cert } = require("firebase-admin/app");
-const { getMessaging } = require("firebase-admin/messaging");
-const serviceAccount = require("./shnoor-job-portal-firebase-adminsdk-fbsvc-39225a8005.json");
-const app = initializeApp({
-  credential: cert(serviceAccount),
-});
-module.exports = {
-  app,
-  messaging: getMessaging(app),
+const admin = require("firebase-admin");
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
 };
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+module.exports = admin;
